dd33a1d06ca81e004483253bb48070c78cdb988d
K_KY <kyuyoungk@naver.com>
2023-04-17 00:26:17 +0900

    Initial commit

---
+++ src/main/java/com/springboot/springbootidu/SpringbootiduApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootiduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootiduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springbootidu/SpringbootiduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootiduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
