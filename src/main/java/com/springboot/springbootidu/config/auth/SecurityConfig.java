6867710ddfb1888a6aaeefa8fff88ea7e25e132c
K_KY <kyuyoungk@naver.com>
2023-04-27 00:08:41 +0900

    SecurityConfig.java 파일에 filterChain 부분을 다음과 같이 고쳐서 권한 문제는 해결했음
    문제는 authorizeRequests() 가 deprecated 되서 발생한 문제였음
    
    index.html 에서 자동으로 로그인 되버리는 문제는 여전히 있음

--- src/main/java/com/springboot/springbootidu/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/springbootidu/config/auth/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.springboot.springbootidu.config.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -15,21 +17,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeRequests()
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/user/**").authenticated()
                 .anyRequest().permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                .and().formLogin().loginPage("/login");
 
         return http.build();
-    }
 
+    }
 }
