02c472e0ad5915cd3e5c8df1c130a3398134891f
K_KY <kyuyoungk@naver.com>
2023-06-10 05:17:47 +0900

    travis ci S3 CodeDeploy 연동 배포 자동화 테스트 네번째 : 해결 안됨 권한이 필요하다 뜬 거에 다 sudo 붙여버림

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@ REPOSITORY=/root/app/step1
 PROJECT_NAME=springbootidu
 
 echo "> Build 파일 복사"
-chmod +rwx /root/app/step1/
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+sudo chmod +rwx /root/app/step1/
+sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 어플리케이션 pid 확인"
 
@@ -21,11 +21,11 @@ else
 fi
 
 echo "> Deploy new application"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | grep -v "plain" | tail -n 1)
+JAR_NAME=$(sudo ls -tr $REPOSITORY/ | grep jar | grep -v "plain" | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar -Dspring.config.location=classpath:/application.properties,/root/app/application-oauth.properties,/root/app/application-real-db.properties,/root/app/step1/springbootidu/src/main/resources/application-real.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
