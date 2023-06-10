0470592ff9ae40e89dee2a0a46102a4f34b3c6d7
K_KY <kyuyoungk@naver.com>
2023-06-10 15:04:12 +0900

    travis ci S3 CodeDeploy 연동 배포 자동화 테스트 17번째 : 해결 안됨 .jar 파일에 실행권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,7 +30,7 @@ sudo chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 sudo chown ec2-user /home/ec2-user/app/step2/nohup.out
-sudo chmod +w /home/ec2-user/app/step2/*
+sudo chmod +x $REPOSITORY/zip/springbootidu-0.0.1-SNAPSHOT.jar
 
 sudo nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
