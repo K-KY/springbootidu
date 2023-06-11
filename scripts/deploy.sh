51d360d4035dcca0da0c92a91bd3eec6f1e90abf
K_KY <kyuyoungk@naver.com>
2023-06-11 15:01:59 +0900

    처음부터 다시 해보자3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,4 +35,3 @@ sudo chmod +x $REPOSITORY/zip/springbootidu-0.0.1-SNAPSHOT.jar
 sudo nohup java -jar \
 -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
  -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
