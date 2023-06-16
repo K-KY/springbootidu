391588bf141b3e336b991687600807e12177da14
K_KY <kyuyoungk@naver.com>
2023-06-16 21:57:59 +0900

    travis-ci 배포 자동화 테스트 경로를 바꿨더니 바뀐 경로로 똑같은 에러가 남 접근권한 문제인것 같아서 한번 줘봄

--- script/deploy.sh
+++ script/deploy.sh
@@ -33,12 +33,13 @@ echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME  실행"
+chmod +x application-real
 
 nohup java -jar \
       -Dspring.config.location=classpath:/application.properties,\
-            /home/ec2-user/app/application-real.properties,\
-            /home/ec2-user/app/application-oauth.properties,\
-            /home/ec2-user/app/application-real-db.properties \
+      classpath:/application-real.properties,\
+      /home/ec2-user/app/application-oauth.properties,\
+      /home/ec2-user/app/application-real-db.properties\
       -Dspring.profiles.active=real \
       $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
