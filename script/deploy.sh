b5eb9eccd3fba061788dd3f73771df88f081de94
K_KY <kyuyoungk@naver.com>
2023-06-16 12:32:07 +0900

    왜 안되지 codedeploy 에도 on branch 추가 해서 해결 함 master 가 아닌 github 브랜치에는 직접 브랜치를 추가 해야하는듯 script/deploy.sh 추가했음

---
+++ script/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=springbootidu
+
+echo "> 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/ # $REPOSITORY/zip 의 확장자가 jar 인 파일을 모두 $REPOSITORY 로 복사
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl springbootidu | grep jar | grep -v "plain" | awk '{print $1}')
+# pgrep :: springbootidu  라는 이름을 가진 프로세스를 검색
+# grep jar :: 위에서 검색한 프로세스 중에서 jar 문자열을 포함하는 프로세스만 필터링
+# grep -v "plain" :: plain 문자열을 포함하지 않는 문자열만 남기고 나머지 제외
+# awk '{print $1}' :: 이전 단계에서 남은 프로세스 중 첫 번째 항목(PID) 만 추출한다\
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리캐이션이 없음"
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v "plain" | tail -n 1)
+# ls -tr $REPOSITORY/*.jar :: REPOSITORY 디렉토리의 모든 .jar 파일을 수정시간에 따라 나열한다 old -> new
+# tail -n 1 :: 위에서 얻은 파일 목록 중 가장 마지막의 파일을 선택한다.
+# grep -v "plain" plain 문자열을 포함하지 않는 문자열만 남기고 나머지 제외
+
+echo "> JAR_NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+echo "> $JAR_NAME  실행"
+
+nohup java -jar \
+      -Dspring.config.location=classpath:/application.properties,\
+      classpath:/application-real.properties,\
+      /home/ec2-user/app/application-oauth.properties,\
+      /home/ec2-user/app/application-real-db.properties\
+      -Dspring.profiles.active=real \
+      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
