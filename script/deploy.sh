fe3a29e0f6a825a4ea5e95c4db9daa62fe5ad8c4
K_KY <kyuyoungk@naver.com>
2023-06-16 21:47:01 +0900

    travis-ci 배포 자동화 테스트 경로를 바꿨더니 바뀐 경로로 똑같은 에러가 남

--- script/deploy.sh
+++ script/deploy.sh
@@ -36,9 +36,9 @@ echo "> $JAR_NAME  실행"
 
 nohup java -jar \
       -Dspring.config.location=classpath:/application.properties,\
-      file:/home/ec2-user/app/application-real.properties,\
-       file:/home/ec2-user/app/application-oauth.properties,\
-      file:/home/ec2-user/app/application-real-db.properties\
+            /home/ec2-user/app/application-real.properties,\
+            /home/ec2-user/app/application-oauth.properties,\
+            /home/ec2-user/app/application-real-db.properties \
       -Dspring.profiles.active=real \
       $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
