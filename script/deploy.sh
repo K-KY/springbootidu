90cd7a3a59edcc14230b866663da7c77095fb5c4
K_KY <kyuyoungk@naver.com>
2023-06-18 22:09:22 +0900

    travis-ci 배포 자동화 테스트 배포 자동화 까진 성공했는데 새로운 문제를 발견함
    2 : 구글 로그인, 네이버 로그인 링크가 고장남
    
    안되는 이유 자체는 찾음
    ReadMe.md 참고

--- script/deploy.sh
+++ script/deploy.sh
@@ -38,5 +38,6 @@ echo "> $JAR_NAME  실행"
 
 #jar name 붙여서 해결
 nohup java -jar \
-      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
