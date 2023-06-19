9717fe87c0fb2c4cdcaac6c78fd0fd31ae7fda9b
K_KY <kyuyoungk@naver.com>
2023-06-19 12:45:52 +0900

    nginx profileController

--- src/main/java/com/springboot/springbootidu/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/springbootidu/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeHttpRequests()
-                .requestMatchers("/user/**","/profile").authenticated()
+                .requestMatchers("/user/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .logout()
